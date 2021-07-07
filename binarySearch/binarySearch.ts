9dec1c664d9fb1fa8fce191595f8751557f2e9f0
Movsar Makhmutov <movsar.makhmutov@auriga.com>
2021-07-08 00:58:24 +0300

    binary search

--- binarySearch/binarySearch.ts
+++ binarySearch/binarySearch.ts
@@ -1,9 +1,9 @@
 const binarySearch = (arr: number[], target: number): number | null => {
   let start = 0,
-    end = arr.length;
+    end = arr.length - 1;
 
   while (start <= end) {
-    let mid = Math.floor(start + end / 2);
+    let mid = Math.floor(end + (start - end) / 2);
 
     if (arr[mid] > target) end = mid - 1;
     else if (arr[mid] < target) start = mid + 1;
@@ -12,4 +12,4 @@ const binarySearch = (arr: number[], target: number): number | null => {
   return null;
 };
 
-binarySearch([1, 2, 3, 4, 7, 10], 2);
+binarySearch([1, 2, 3, 4, 7, 10], 10);
